0193c689f75ac80002be405950d64ed1cfb0cd35
Rohit Kumar <rohittourister@gmail.com>
2023-01-21 13:35:59 +0530

    Added refactored code

--- dpk.js
+++ dpk.js
@@ -3,26 +3,15 @@ const crypto = require("crypto");
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
-
+  let partitionKey = TRIVIAL_PARTITION_KEY;
   if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
+    partitionKey = event.partitionKey || JSON.stringify(event);
+    if (typeof partitionKey !== "string") {
+      partitionKey = JSON.stringify(partitionKey);
+    }
+    if (partitionKey.length > MAX_PARTITION_KEY_LENGTH) {
+      partitionKey = crypto.createHash("sha3-512").update(partitionKey).digest("hex");
     }
   }
-
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+  return partitionKey;
 };
