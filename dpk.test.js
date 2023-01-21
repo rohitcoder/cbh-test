e7124877f79404370a9597dca02285801e9e4975
Rohit Kumar <rohittourister@gmail.com>
2023-01-21 13:42:58 +0530

    Added new test cases

--- dpk.test.js
+++ dpk.test.js
@@ -5,4 +5,21 @@ describe("deterministicPartitionKey", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+
+   // test case for when event is null
+   it("returns 0 when event is null", () => {
+    expect(deterministicPartitionKey(null)).toEqual("0");
+  });
+
+  // test case for when event.partitionKey is a string and less than 256 characters
+  it("returns event.partitionKey when it is a string and less than 256 characters", () => {
+    expect(deterministicPartitionKey({ partitionKey: "abc" })).toEqual("abc");
+  });
+
+  // test case for when event.partitionKey is not present
+  it("returns sha3-512 hash of event when partitionKey is not present", () => {
+    const event = { a: 1, b: 2 };
+    const hash = crypto.createHash("sha3-512").update(JSON.stringify(event)).digest("hex");
+    expect(deterministicPartitionKey(event)).toEqual(hash);
+  });
 });
