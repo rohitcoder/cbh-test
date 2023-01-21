0aa21388dd80acc57c831f2d9823c06337b43442
Rohit Kumar <rohittourister@gmail.com>
2023-01-21 13:39:30 +0530

    Added some sample execution scenarios in index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const {deterministicPartitionKey} = require("./dpk");
 
-console.log(deterministicPartitionKey());
+console.log("Without Event Data: "+deterministicPartitionKey());
+console.log("With Event Data :"+deterministicPartitionKey({partitionKey: "1234567890"}));
+console.log("With Very long Event Data: "+deterministicPartitionKey({partitionKey: "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"}));
